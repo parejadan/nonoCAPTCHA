f456b2ad4a49f4fa646c12b1fc35e9e274257814
Daniel Pareja <ParejaDan@gmail.com>
2019-02-17 19:38:00 -0500

    fix demo with WSL env + no proxy

--- examples/demo.py
+++ examples/demo.py
@@ -4,13 +4,12 @@ from nonocaptcha.solver import Solver
 pageurl = "https://www.google.com/recaptcha/api2/demo"
 sitekey = "6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-"
 
-proxy = "127.0.0.1:1000"
-auth_details = {"username": "user", "password": "pass"}
-args = ["--timeout 5"]
-options = {"ignoreHTTPSErrors": True, "args": args}
+options = {
+    "ignoreHTTPSErrors": True,
+    "args": ["--timeout 5", "--no-sandbox"],
+    "executablePath":"/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"}
 client = Solver(
-    pageurl, sitekey, options=options, proxy=proxy, proxy_auth=auth_details
-)
+    pageurl, sitekey, options=options)
 
 solution = asyncio.get_event_loop().run_until_complete(client.start())
 if solution:
